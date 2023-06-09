cf93714bf66cd7f666656d7c24bd1bb69b12f0a3
parvejkhan-dev <kparvej761@gmail.com>
2023-06-09 21:42:16 +0530

    session refector app.js

--- config.js
+++ config.js
@@ -1,14 +1,13 @@
 // Import the functions you need from the SDKs you need
 
 const fs = require('firebase-admin');
-const serviceAccount = require('./routes/firebase_auth.json');
+const serviceAccount = require('./firebase_auth.json');
 const Timestamp = fs.firestore.Timestamp;
 
 fs.initializeApp({
   credential: fs.credential.cert(serviceAccount),
   databaseURL: "https://test-assignment-894db-default-rtdb.asia-southeast1.firebasedatabase.app",
   authDomain: "test-assignment-894db.firebaseapp.com",
-
 });
 
 
